b8def1f9d8f90b4a9fed7ceccce95fb28b250907
Floyd <H4K3ROM@Gmail.com>
2018-12-31 16:24:22 -0500

    Bringup H4K3RScript
    Change-Id: Ia6c6fd71a1faeea408cb8a4095873709d3a23898

---
+++ h4k3rom.sh
@@ -0,0 +1,9 @@
+cd ~/H4K3ROM/vendor/gzosp/H4K3RScript
+head -25 README.md
+cd ~/H4K3ROM/device
+mkdir H4K3ROM/device/oneplus
+cd ~/H4K3ROM/device/oneplus
+git clone https://github.com/FloydFlivercod/oneplus3.git
+cd ~/H4K3ROM/vendor/gzosp/H4K3RScript
+head -25 README2.md
+cd ~/H4K3ROM
